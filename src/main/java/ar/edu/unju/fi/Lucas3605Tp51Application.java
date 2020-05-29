8da3e98c9b1293b90092008cbdf09396b596d2d9
Fernando <deluk231lukkah@gmail.com>
2020-05-29 18:34:39 -0300

    Asunto: Inicialización del proyecto Responsabe: Lucas Luis Fernando Task00: Inicialización del Proyecto

---
+++ src/main/java/ar/edu/unju/fi/Lucas3605Tp51Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lucas3605Tp51Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lucas3605Tp51Application.class, args);
+	}
+
+}
